bcada2451b755c1f36b3a4e83dd02b556da5e10e
NikitoriusMinsk <nikitoriusminsk@gmail.com>
2023-04-27 12:03:34 +0400

    init cleanup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,9 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 
-import "@/styles/globals.css";
+import "@/styles/globals.scss";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+	return <Component {...pageProps} />;
 };
 
 export default MyApp;
