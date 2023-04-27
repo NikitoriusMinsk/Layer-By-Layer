e9711732dadb63897f72e154ff298dae093e47ca
NikitoriusMinsk <nikitoriusminsk@gmail.com>
2023-04-27 13:50:06 +0400

    add font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,20 @@
 import { type AppType } from "next/dist/shared/lib/utils";
-
-import "@/styles/globals.scss";
+import "@styles/globals.scss";
 import { appWithTranslation } from "next-i18next";
-import MainLayout from "@/components/layout/MainLayout";
+import MainLayout from "@components/layout/MainLayout";
+import { Montserrat } from "next/font/google";
+
+const montserrat = Montserrat({ subsets: ["cyrillic"] });
 
 const MyApp: AppType = ({ Component, pageProps }) => {
 	return (
-		<MainLayout>
-			<Component {...pageProps} />
-		</MainLayout>
+		<>
+			<div className={montserrat.className}>
+				<MainLayout>
+					<Component {...pageProps} />
+				</MainLayout>
+			</div>
+		</>
 	);
 };
 
