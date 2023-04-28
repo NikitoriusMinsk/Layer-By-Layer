6f9978a94bdd85186b84d0b0dc083b0670d87f7c
NikitoriusMinsk <nikitoriusminsk@gmail.com>
2023-04-28 14:53:34 +0400

    add map to the footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -61,6 +61,13 @@ const Footer: React.FC = () => {
 				<a href="#order">{t("order")}</a>
 				<a href="#contacts">{t("contacts")}</a>
 				<a href="#partnership">{t("partnership")}</a>
+				<iframe
+					src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d21421.90645960415!2d44.81410641647915!3d41.69570049089088!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40440cf966877377%3A0x5982a28de6451cda!2sAvlabari!5e0!3m2!1sru!2sge!4v1682679064033!5m2!1sru!2sge"
+					style={{ border: 0 }}
+					allowFullScreen={false}
+					loading="lazy"
+					referrerPolicy="no-referrer-when-downgrade"
+				></iframe>
 			</div>
 			<div className={styles.copyright}>
 				<span>©2023 All Right reserverd. Layer By Layer Inc.</span>
