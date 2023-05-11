5fee54b9e262ea4c51be053187fd1a4eb152bdd1
NikitoriusMinsk <nikitoriusminsk@gmail.com>
2023-05-11 12:17:13 +0400

    fix eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,6 +12,9 @@ const config = {
             parserOptions: {
                 project: path.join(__dirname, "tsconfig.json"),
             },
+            rules: {
+                "@typescript-eslint/no-unnecessary-type-assertion": "warn",
+            }
         },
     ],
     parser: "@typescript-eslint/parser",
@@ -28,7 +31,6 @@ const config = {
                 fixStyle: "inline-type-imports",
             },
         ],
-        "@typescript-eslint/no-unnecessary-type-assertion": ["warn"],
         "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
     },
 };
