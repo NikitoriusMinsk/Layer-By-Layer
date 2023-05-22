dab81c8649f08c1c53e9eef14a88bbef37bb32b2
NikitoriusMinsk <nikitoriusminsk@gmail.com>
2023-05-22 18:32:09 +0400

    add algorithm images

--- src/components/ui/Algorithm.tsx
+++ src/components/ui/Algorithm.tsx
@@ -14,49 +14,49 @@ const Algorithm: React.FC = () => {
 			<h2>{t("algorithm.heading")}</h2>
 			<div className={styles.content}>
 				<Panel
-					src="/images/algorithm/1.png"
+					src="/images/algorithm/1.svg"
 					alt={t("algorithm.1.alt")}
 					title={t("algorithm.1.title")}
 					text={t("algorithm.1.text")}
 				/>
 				<Panel
-					src="/images/algorithm/2.png"
+					src="/images/algorithm/2.svg"
 					alt={t("algorithm.2.alt")}
 					title={t("algorithm.2.title")}
 					text={t("algorithm.2.text")}
 				/>
 				<Panel
-					src="/images/algorithm/3.png"
+					src="/images/algorithm/3.svg"
 					alt={t("algorithm.3.alt")}
 					title={t("algorithm.3.title")}
 					text={t("algorithm.3.text")}
 				/>
 				<Panel
-					src="/images/algorithm/4.png"
+					src="/images/algorithm/4.svg"
 					alt={t("algorithm.4.alt")}
 					title={t("algorithm.4.title")}
 					text={t("algorithm.4.text")}
 				/>
 				<Panel
-					src="/images/algorithm/5.png"
+					src="/images/algorithm/5.svg"
 					alt={t("algorithm.5.alt")}
 					title={t("algorithm.5.title")}
 					text={t("algorithm.5.text")}
 				/>
 				<Panel
-					src="/images/algorithm/6.png"
+					src="/images/algorithm/6.svg"
 					alt={t("algorithm.6.alt")}
 					title={t("algorithm.6.title")}
 					text={t("algorithm.6.text")}
 				/>
 				<Panel
-					src="/images/algorithm/7.png"
+					src="/images/algorithm/7.svg"
 					alt={t("algorithm.7.alt")}
 					title={t("algorithm.7.title")}
 					text={t("algorithm.7.text")}
 				/>
 				<Panel
-					src="/images/algorithm/4.png"
+					src="/images/algorithm/8.svg"
 					alt={t("algorithm.8.alt")}
 					title={t("algorithm.8.title")}
 					text={t("algorithm.8.text")}
@@ -77,8 +77,8 @@ const Panel: React.FC<{ src: string; alt: string; title: string; text: string }>
 				<Image
 					src={src}
 					alt={alt}
-					height={150}
-					width={150}
+					height={50}
+					width={50}
 				/>
 			</div>
 			<div>
